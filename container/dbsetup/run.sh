42bd7721a6642de40de90745326ea10a699e7d26
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2019-11-24 18:20:48 +0100

    dbsetup: always deploy data from newest dump

--- container/dbsetup/run.sh
+++ container/dbsetup/run.sh
@@ -26,28 +26,29 @@ log_info "run athene2 dbsetup version $VERSION revision $GIT_REVISION"
 # add nameserver as currently alpine images dns seems not to work properly in GKE
 cat /etc/resolv.conf | grep 1.1.1.1 || printf "\nnameserver 1.1.1.1\n" >> /etc/resolv.conf
 
+connect="-h $ATHENE2_DATABASE_HOST --port $ATHENE2_DATABASE_PORT -u $ATHENE2_DATABASE_USER -p$ATHENE2_DATABASE_PASSWORD"
+
+log_info "wait for athene2 database to be ready"
+until mysql $connect -e "SHOW DATABASES" >/dev/null 2>/dev/null
+do
+    log_warn "could not find athene2 server - retry in 10 seconds"
+    sleep 10
+done
+
 if [[ ! -f /tmp/dump.zip ]] ; then
     if [[ "${GCLOUD_BUCKET_URL}" != "" ]] ; then
         echo ${GCLOUD_SERVICE_ACCOUNT_KEY} >/tmp/service_account_key.json
         gcloud auth activate-service-account ${GCLOUD_SERVICE_ACCOUNT_NAME} --key-file /tmp/service_account_key.json
-        bucket_file=$(gsutil ls -l gs://anonymous-data | grep dump | sort -k 2 | awk '{ print $3 }')
+        bucket_file=$(gsutil ls -l gs://anonymous-data | grep dump | sort -rk 2 | sed 1q | awk '{ print $3 }')
         if [[ "${bucket_file}" == "" ]] ; then
             log_warn "no anonymous database dump available in gs://anonymous-data"
             exit 1
         fi
         gsutil cp ${bucket_file} /tmp/dump.zip
         log_info "latest dump ${bucket_file} downloaded from serlo-shared"
+        mysql $connect serlo -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE IF EXISTS uuid; DROP TABLE IF EXISTS event_parameter_uuid"
     fi
 fi
-
-connect="-h $ATHENE2_DATABASE_HOST --port $ATHENE2_DATABASE_PORT -u $ATHENE2_DATABASE_USER -p$ATHENE2_DATABASE_PASSWORD"
-
-log_info "wait for athene2 database to be ready"
-until mysql $connect -e "SHOW DATABASES" >/dev/null 2>/dev/null
-do 
-    log_warn "could not find athene2 server - retry in 10 seconds"
-    sleep 10
-done
 
 for retry in 1 2 3 4 5 6 7 8 9 10 ; do
     log_info "check if athene2 database exists"
@@ -65,8 +66,8 @@ for retry in 1 2 3 4 5 6 7 8 9 10 ; do
             retry
             continue
         fi
-            
-        mysql $connect </tmp/dump.sql
+
+        mysql $connect serlo </tmp/dump.sql
         if [[ $? != 0 ]] ; then
             log_warn "import dump failed" ; retry
             continue
