34242d9fbe019107347b5d455bc8a4283ecfad60
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-02-21 11:14:20 +0100

    feat(dbsetup): modify also postgres kratos database

--- container/dbsetup/run.sh
+++ container/dbsetup/run.sh
@@ -1,59 +1,60 @@
 #!/bin/sh
 
-log_info() {
-    time=$(date +"%Y-%m-%dT%H:%M:%SZ")
-    echo "{\"level\":\"info\",\"time\":\"$time\",\"message\":\"$1\"}"
-}
+source ./utils.sh
 
-log_fatal() {
-    time=$(date +"%Y-%m-%dT%H:%M:%SZ")
-    echo "{\"level\":\"fatal\",\"time\":\"$time\",\"message\":\"$1\"}"
-}
-
-log_warn() {
-    time=$(date +"%Y-%m-%dT%H:%M:%SZ")
-    echo "{\"level\":\"warn\",\"time\":\"$time\",\"message\":\"$1\"}"
-}
-
-retry() {
-    seconds=30
-    log_info "retrying in ${seconds} seconds"
-    sleep ${seconds}
-}
-
-log_info "run athene2 dbsetup version $VERSION revision $GIT_REVISION"
+log_info "run dbsetup version $VERSION revision $GIT_REVISION"
 
 # add nameserver as currently alpine images dns seems not to work properly in GKE
-cat /etc/resolv.conf | grep 1.1.1.1 || printf "\nnameserver 1.1.1.1\n" >> /etc/resolv.conf
+cat /etc/resolv.conf | grep 1.1.1.1 || printf "\nnameserver 1.1.1.1\n" >>/etc/resolv.conf
 
-connect="-h $ATHENE2_DATABASE_HOST --port $ATHENE2_DATABASE_PORT -u $ATHENE2_DATABASE_USER -p$ATHENE2_DATABASE_PASSWORD"
+mysql_connect="--host=${MYSQL_HOST} --port=${MYSQL_PORT} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD}"
 
-log_info "wait for athene2 database to be ready"
-until mysql $connect -e "SHOW DATABASES" >/dev/null 2>/dev/null
-do
-    log_warn "could not find athene2 server - retry in 10 seconds"
+log_info "wait for mysql database to be ready"
+until mysql $mysql_connect -e "SHOW DATABASES" >/dev/null 2>/dev/null; do
+    log_warn "could not find mysql server - retry in 10 seconds"
     sleep 10
 done
 
 log_info "create serlo database if it's not there yet"
-mysql $connect -e "CREATE DATABASE IF NOT EXISTS serlo"
+mysql $mysql_connect -e "CREATE DATABASE IF NOT EXISTS serlo"
 
-[ -z "GCLOUD_BUCKET_URL" ] && { log_fatal "GCLOUD_BUCKET_URL not given"; exit 1; }
+[ -z "GCLOUD_BUCKET_URL" ] && {
+    log_fatal "GCLOUD_BUCKET_URL not given"
+    exit 1
+}
 
-echo $GCLOUD_SERVICE_ACCOUNT_KEY > /tmp/service_account_key.json
+echo $GCLOUD_SERVICE_ACCOUNT_KEY >/tmp/service_account_key.json
 gcloud auth activate-service-account ${GCLOUD_SERVICE_ACCOUNT_NAME} --key-file /tmp/service_account_key.json
 newest_dump_uri=$(gsutil ls -l gs://anonymous-data | grep dump | sort -rk 2 | head -n 1 | awk '{ print $3 }')
-[ -z "$newest_dump_uri" ] && { log_fatal "no database dump available in gs://anonymous-data"; exit 1; }
+[ -z "$newest_dump_uri" ] && {
+    log_fatal "no database dump available in gs://anonymous-data"
+    exit 1
+}
 newest_dump=$(basename $newest_dump_uri)
 [ -f "/tmp/$newest_dump" ] && exit 0
 
 gsutil cp $newest_dump_uri "/tmp/$newest_dump"
 log_info "downloaded newest dump $newest_dump"
-unzip -o "/tmp/$newest_dump" -d /tmp || { log_fatal "unzip of dump file failed"; exit 1; }
-mysql $connect serlo < "/tmp/dump.sql" || { log_fatal "import of dump failed"; exit 1; }
-mysql $connect -e "LOAD DATA LOCAL INFILE '/tmp/user.csv' INTO TABLE user FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" serlo || { log_fatal "import of dump failed"; exit 1; }
+unzip -o "/tmp/$newest_dump" -d /tmp || {
+    log_fatal "unzip of dump file failed"
+    exit 1
+}
+mysql $mysql_connect serlo <"/tmp/mysql.sql" || {
+    log_fatal "import of dump failed"
+    exit 1
+}
+mysql $mysql_connect -e "LOAD DATA LOCAL INFILE '/tmp/user.csv' INTO TABLE user FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 ROWS;" serlo || {
+    log_fatal "import of dump failed"
+    exit 1
+}
 log_info "imported serlo database dump $newest_dump"
+
+export PGPASSWORD=$POSTGRES_PASSWORD
+postgres_connect="--host=${POSTGRES_HOST} --user=serlo kratos"
+psql $postgres_connect -c "DROP DATABASE kratos"
+psql $postgres_connect -c "CREATE DATABASE kratos"
+psql $postgres_connect <postgres.sql
 
 # delete all unnecessary files
 rm -f $(ls /tmp/dump*.zip | grep -v $newest_dump)
-rm /tmp/dump.sql /tmp/user.csv
+rm /tmp/*.sql /tmp/user.csv
